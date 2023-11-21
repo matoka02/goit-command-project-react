77c2b0233e49479d7d4eacf540d4d6d4e6ef5519
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-11-21 12:15:32 +0200

    create components
    - PrivateRoute;
    - PublicRoute.

---
+++ src/components/PublicRoute/PublicRoute.jsx
@@ -0,0 +1,9 @@
+import { useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
+import { selectToken } from 'redux/auth/authSelectors';
+
+export const PrivateRoute = ({ component, redirectTo = '/' }) => {
+  const isLoggedIn = useSelector(selectToken);
+
+  return !isLoggedIn ? component : <Navigate to={redirectTo} />
+};
