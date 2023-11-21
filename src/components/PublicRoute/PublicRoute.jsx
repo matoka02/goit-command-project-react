615b155f358452a68f90b1026fe3bb70b941c38c
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-11-21 17:53:35 +0200

    Update PublicRoute.jsx

--- src/components/PublicRoute/PublicRoute.jsx
+++ src/components/PublicRoute/PublicRoute.jsx
@@ -2,7 +2,7 @@ import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 import { selectToken } from 'redux/auth/authSelectors';
 
-export const PrivateRoute = ({ component, redirectTo = '/' }) => {
+export const PublicRoute = ({ component, redirectTo = '/' }) => {
   const isLoggedIn = useSelector(selectToken);
 
   return !isLoggedIn ? component : <Navigate to={redirectTo} />
