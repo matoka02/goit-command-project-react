3da4abc47d80810d587e3f34c9c703bd844a50c9
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-11-21 14:13:41 +0200

    create pages
    - CalculatorPage;
    - LoginPage;
    - MainPage;
    - RegistrationPage.

---
+++ src/Pages/CalculatorPage/CalculatorPage.styled.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  @media screen and (min-width: 1280px) {
+    display: flex;
+    justify-content: space-between;
+  }
+`;
